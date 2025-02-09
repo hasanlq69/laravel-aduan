c63b6542cb84499eaa75d56c345400e821ac22e0
hasanlq69 <hasanlq69@gmail.com>
2025-02-09 08:09:57 +0700

    installing dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
 import '../css/app.css';
+import "@vueform/multiselect/themes/default.css";
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -18,6 +21,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(CKEditor)
             .use(ZiggyVue)
             .mount(el);
     },
